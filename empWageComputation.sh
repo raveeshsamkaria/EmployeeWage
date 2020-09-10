e25b6c441e231fdcd2abeed978539cb9689a3225
Raveesh Samkaria <rishu9417@gmail.com>
2020-09-10 16:31:27 +0530

    Added : Employee Attendance functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 echo ">---< Employee Wage Computation >---<"
+
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+if [ $isPresent -eq $randomCheck ]
+then
+        echo "Employee is Present"
+else
+        echo "Not Presnet"
+fi
