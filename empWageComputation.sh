f27b83c2ee2b7097f365c198c01230998b462396
Raveesh Samkaria <rishu9417@gmail.com>
2020-09-10 16:45:40 +0530

    Added : Employee Daily Wage functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 echo ">---< Employee Wage Computation >---<"
 
+wagePerHour=20
+fullWorkingHours=16
 isPresent=1
 randomCheck=$(( RANDOM%2 ))
 if [ $isPresent -eq $randomCheck ]
 then
         echo "Employee is Present"
 else
-        echo "Not Presnet"
+        echo "Not Present"
 fi
+
+dailyWage=$(( $wagePerHour * $fullWorkingHours ))
+echo "Daily Wages : $dailyWage"
